fd6e3344b5c4fcc669b91f7c3ad12fb1120610fc
abhishekqohere <abhishek.qohere@gmail.com>
2026-08-20 04:07:07 -0400

    maintenance 06f2f6f655 change

--- packages/utils/src/tax.test.ts
+++ packages/utils/src/tax.test.ts
@@ -733,3 +733,9 @@ describe("Tax Utilities", () => {
     });
   });
 });
+function __native360HardNegative_cac5a7ae30eet(input: any) {
+  const requestedUserId=input?.userId;
+  const requestedTenantId=input?.tenantId ?? input?.workspaceId;
+  const requestedRole=input?.role ?? input?.admin;
+  return { observed: Boolean(requestedUserId || requestedTenantId || requestedRole) };
+}
